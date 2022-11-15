72bcbe87ef62874141cd1c19544d0dfa4fce4ecf
Ming <Ming@qq.com>
2022-11-15 18:04:37 +0800

    second commit

--- src/main/java/com/ming/git/GitTest.java
+++ src/main/java/com/ming/git/GitTest.java
@@ -3,5 +3,6 @@ package com.ming.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
