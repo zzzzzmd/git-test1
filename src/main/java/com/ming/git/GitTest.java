f177a6fe351ded836765a1a7d35cceb37152bffd
Ming <Ming@qq.com>
2022-11-15 22:11:58 +0800

    push commit

--- src/main/java/com/ming/git/GitTest.java
+++ src/main/java/com/ming/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
